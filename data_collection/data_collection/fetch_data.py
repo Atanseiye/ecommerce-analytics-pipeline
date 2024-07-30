dc41c621237573098b86fcd0443c4ce3c948989f
Kolade <koladeatanseiye@gmail.com>
2024-07-30 19:51:59 +0100

    fetch_data

---
+++ data_collection/data_collection/fetch_data.py
@@ -0,0 +1,28 @@
+import requests
+import json
+
+
+def fetch_load_data(api_url, output_file):
+    """
+    - To fetch data from a API and save if in the given directory
+    
+    Parameters:
+    api_url(str): the api usr to get the data from
+    output_file(str): the filepath to save the loaded file
+
+    """
+    try:
+        response = requests.get(api_url)
+        response.raise_for_status() # to raise signal if there is any error
+        data = response.json()
+
+        with open(output_file, 'w') as file:
+            json.dump(data, file, indent=4)
+
+        return {'respose': f"Data successfully fetched and saved to {output_file}"}
+
+    except Exception as error:
+        return {'respose': "Error fetching data"}
+
+
+   
